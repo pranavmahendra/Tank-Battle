daf76f58d5c6dff1620b720d88f4379eb552fd11
Pranav Mahendra <>
2020-04-03 23:29:59 +0530

    Created a singleton generic and a tank service from that singeton generic class.

---
+++ Assets/Scripts/Tank MVC/TankService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankService : MonosingletonGeneric<TankService>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        Debug.Log("This message is from Tank Service");
+    }
+}
